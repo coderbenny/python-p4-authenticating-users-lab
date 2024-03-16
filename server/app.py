ccb3c0a2b4e4582536adc42b735765de3dccfb1b
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-16 22:34:52 +0300

    done

--- server/app.py
+++ server/app.py
@@ -48,9 +48,44 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return jsonify(user.to_dict())
+
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    
+    def delete(self):
+        session['user_id'] = None
+        return '', 204
+
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
 
 
 if __name__ == '__main__':
